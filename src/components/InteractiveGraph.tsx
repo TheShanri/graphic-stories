a19a64741dca90b2a553cd7ede6b726103a70f61
Shahriar Akhavan Tafti <theexaminedexistence@gmail.com>
2025-12-11 18:06:21 -0500

    Remove invalid xmlns from node tooltip

--- src/components/InteractiveGraph.tsx
+++ src/components/InteractiveGraph.tsx
@@ -374,7 +374,6 @@ function InteractiveGraph({ graph, onNodeClick }: Props) {
               height={cardHeight}
             >
               <div
-                xmlns="http://www.w3.org/1999/xhtml"
                 style={{
                   width: '100%',
                   height: '100%',
